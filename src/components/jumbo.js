2d18b3ef33c2f106d087b88337f84376c2042c75
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-07-09 14:14:20 -0500

    first commit

---
+++ src/components/jumbo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyledJumbo } from '../styles/components';
+
+export default function jumbo() {
+  return (
+    <StyledJumbo>
+      <div>
+        <h2>Get the best exclusive and special swag from Platzi!</h2>
+        <small></small>
+      </div>
+    </StyledJumbo>
+  );
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -5,12 +5,12 @@
  * See: https://www.gatsbyjs.org/docs/use-static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+import React from 'react';
+import PropTypes from 'prop-types';
+import { useStaticQuery, graphql } from 'gatsby';
 
-import Header from "./header"
-import "./layout.css"
+import Header from './header';
+import './layout.css';
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -21,7 +21,7 @@ const Layout = ({ children }) => {
         }
       }
     }
-  `)
+  `);
 
   return (
     <>
@@ -38,15 +38,15 @@ const Layout = ({ children }) => {
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href='https://www.gatsbyjs.org'>Gatsby</a>
         </footer>
       </div>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
